e8e7287277a4549aa7a45c8bcddab7604cbc01ea
demirose41 <demirose41@gmail.com>
2024-10-07 19:20:24 -0700

    1:initial commit

--- error-types/validation-error.js
+++ error-types/validation-error.js
@@ -1,4 +1,14 @@
-// Your code here
+class ValidationError extends Error {
+  constructor(message, ...params){
+    super(...params);
+
+    if(Error.captureStackTrace){
+      Error.captureStackTrace(this, ValidationError);
+    }
+    this.name = 'ValidationError';
+    this.message = message || 'Invalid input';
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
